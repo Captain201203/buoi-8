7c862d87b15d9d3a886bf29929a1d69a00414131
Captain201203 <vuhoangan010104@gmail.com>
2024-10-21 15:44:51 +0700

    buoi8 project

---
+++ source/repos/dâtbinding/dâtbinding/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace dâtbinding
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbId = new System.Windows.Forms.TextBox();
+            this.txbName = new System.Windows.Forms.TextBox();
+            this.txbFaculty = new System.Windows.Forms.TextBox();
+            this.txbAverage = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txbId
+            // 
+            this.txbId.Location = new System.Drawing.Point(173, 122);
+            this.txbId.Name = "txbId";
+            this.txbId.Size = new System.Drawing.Size(352, 20);
+            this.txbId.TabIndex = 0;
+            // 
+            // txbName
+            // 
+            this.txbName.Location = new System.Drawing.Point(173, 226);
+            this.txbName.Name = "txbName";
+            this.txbName.Size = new System.Drawing.Size(352, 20);
+            this.txbName.TabIndex = 1;
+            // 
+            // txbFaculty
+            // 
+            this.txbFaculty.Location = new System.Drawing.Point(173, 325);
+            this.txbFaculty.Name = "txbFaculty";
+            this.txbFaculty.Size = new System.Drawing.Size(352, 20);
+            this.txbFaculty.TabIndex = 2;
+            // 
+            // txbAverage
+            // 
+            this.txbAverage.Location = new System.Drawing.Point(173, 425);
+            this.txbAverage.Name = "txbAverage";
+            this.txbAverage.Size = new System.Drawing.Size(352, 20);
+            this.txbAverage.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(638, 122);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(542, 333);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrevious.Location = new System.Drawing.Point(749, 490);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(104, 38);
+            this.btnPrevious.TabIndex = 5;
+            this.btnPrevious.Text = "Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNext.Location = new System.Drawing.Point(987, 490);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(104, 38);
+            this.btnNext.TabIndex = 6;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(73, 490);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(104, 38);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(243, 490);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(104, 38);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Xóa";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(421, 490);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(104, 38);
+            this.btnUpdate.TabIndex = 9;
+            this.btnUpdate.Text = "Sửa";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1211, 593);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txbAverage);
+            this.Controls.Add(this.txbFaculty);
+            this.Controls.Add(this.txbName);
+            this.Controls.Add(this.txbId);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbId;
+        private System.Windows.Forms.TextBox txbName;
+        private System.Windows.Forms.TextBox txbFaculty;
+        private System.Windows.Forms.TextBox txbAverage;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ source/repos/dâtbinding/dâtbinding/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+using System.Xml.Linq;
+
+namespace dâtbinding
+{
+    public partial class Form1 : Form
+    {
+        private int currentRowIndex = -1;
+        private BindingSource sachBindingSource = new BindingSource();
+        private Model1 db;
+        public Form1()
+        {
+            InitializeComponent();
+            db = new Model1();
+        }
+        public void LoadData(DataGridView dgv)
+        {
+            using (var db = new Model1())
+            {
+                var students = db.Students
+                                 .Include(s => s.Major)  
+                                 .Select(s => new
+                                 {
+                                     s.mssv,
+                                     s.tenSinhVien,
+                                     FacultyName = s.Faculty != null ? s.Faculty.facultyName : "Chưa có ngành",
+                                     s.diemTB,
+                                 }).ToList();
+
+                dgv.DataSource = students;
+            }
+        }
+
+        public void DataBinding(DataGridView dgv, TextBox txtMssv, TextBox txtTenSinhVien, TextBox txtDiemTB, TextBox txtFaculty)
+        {
+           
+            txtMssv.DataBindings.Clear();
+            txtMssv.DataBindings.Add("Text", dgv.DataSource, "Mssv");
+
+            txtTenSinhVien.DataBindings.Clear();
+            txtTenSinhVien.DataBindings.Add("Text", dgv.DataSource, "TenSinhVien");
+
+            txtDiemTB.DataBindings.Clear();
+            txtDiemTB.DataBindings.Add("Text", dgv.DataSource, "DiemTB");
+
+            txtFaculty.DataBindings.Clear();
+            txtFaculty.DataBindings.Add("Text", dgv.DataSource, "FacultyName"); // Binding với FacultyName
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData(dataGridView1);
+            if (dataGridView1.Rows.Count > 0)
+            {
+                currentRowIndex = 0; // Đặt chỉ số hàng hiện tại về 0
+                LoadStudentData(currentRowIndex); // Tải dữ liệu cho sinh viên đầu tiên
+            }
+
+            DataBinding(dataGridView1, txbId, txbName, txbAverage, txbFaculty);
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count > 0)
+            {
+                if (currentRowIndex > 0)
+                {
+                    currentRowIndex--;
+                    dataGridView1.Rows[currentRowIndex].Selected = true;
+                    dataGridView1.FirstDisplayedScrollingRowIndex = currentRowIndex;
+                    LoadStudentData(currentRowIndex); // Đảm bảo đây là cách gọi đúng
+                }
+            }
+        }
+        private void LoadStudentData(int rowIndex)
+        {
+            if (rowIndex >= 0 && rowIndex < dataGridView1.Rows.Count)
+            {
+                DataGridViewRow row = dataGridView1.Rows[rowIndex];
+                txbId.Text = row.Cells["mssv"].Value.ToString();
+                txbName.Text = row.Cells["tenSinhVien"].Value.ToString();
+                txbAverage.Text = row.Cells["diemTB"].Value.ToString();
+                txbFaculty.Text = row.Cells["FacultyName"].Value.ToString();
+            }
+        }
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+           
+            if (dataGridView1.Rows.Count > 0)
+            {
+                // Kiểm tra nếu hàng hiện tại là hàng cuối
+                if (currentRowIndex < dataGridView1.Rows.Count - 1)
+                {
+                    currentRowIndex++; // Tăng chỉ số hàng hiện tại
+                    dataGridView1.Rows[currentRowIndex].Selected = true; // Chọn hàng hiện tại
+                    dataGridView1.FirstDisplayedScrollingRowIndex = currentRowIndex; // Cuộn đến hàng hiện tại
+                    LoadStudentData(currentRowIndex); // Tải dữ liệu sinh viên
+                }
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (var db = new Model1())
+            {
+                // Tạo một đối tượng sinh viên mới
+                var newStudent = new Student
+                {
+                    mssv = txbId.Text,
+                    tenSinhVien = txbName.Text,
+                    diemTB = Convert.ToDouble(txbAverage.Text),
+                    // Đặt facultyId cho ngàn nếu cần
+                    facultyId = db.Faculties.FirstOrDefault(f => f.facultyName == txbFaculty.Text)?.facultyId // Tìm kiếm facultyId dựa trên tên
+                };
+
+                // Thêm sinh viên vào cơ sở dữ liệu
+                db.Students.Add(newStudent);
+                db.SaveChanges();
+
+                // Tải lại dữ liệu vào DataGridView
+                LoadData(dataGridView1);
+                MessageBox.Show("Thêm sinh viên thành công!");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            using (var db = new Model1())
+            {
+                // Lấy mã sinh viên từ TextBox
+                var mssvToDelete = txbId.Text;
+
+                // Tìm sinh viên cần xóa
+                var student = db.Students.FirstOrDefault(s => s.mssv == mssvToDelete);
+                if (student != null)
+                {
+                    db.Students.Remove(student);
+                    db.SaveChanges();
+
+                    // Tải lại dữ liệu vào DataGridView
+                    LoadData(dataGridView1);
+                    MessageBox.Show("Xóa sinh viên thành công!");
+                }
+                else
+                {
+                    MessageBox.Show("Không tìm thấy sinh viên với MSSV này.");
+                }
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            using (var db = new Model1())
+            {
+                // Lấy mã sinh viên từ TextBox
+                var mssvToEdit = txbId.Text;
+
+                // Tìm sinh viên cần sửa
+                var student = db.Students.FirstOrDefault(s => s.mssv == mssvToEdit);
+                if (student != null)
+                {
+                    student.tenSinhVien = txbName.Text;
+                    student.diemTB = Convert.ToDouble(txbAverage.Text);
+                    student.facultyId = db.Faculties.FirstOrDefault(f => f.facultyName == txbFaculty.Text)?.facultyId; // Cập nhật facultyId
+
+                    db.SaveChanges();
+
+                    // Tải lại dữ liệu vào DataGridView
+                    LoadData(dataGridView1);
+                    MessageBox.Show("Sửa thông tin sinh viên thành công!");
+                }
+                else
+                {
+                    MessageBox.Show("Không tìm thấy sinh viên với MSSV này.");
+                }
+            }
+        }
+    }
+}
